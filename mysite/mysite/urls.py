fb55d1e220050e7c7f618d51d762320f0f595762
dastanulukbekovfrontend-crypto <dastanulukbekovbackend@gmail.com>
2025-12-30 17:36:33 +0300

    azon

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,7 +20,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = i18n_patterns(
-    path('admin/', admin.site.urls),
+   path('admin/', admin.site.urls),
     path('', include('movie_app.urls')),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('accounts/', include('allauth.urls')),
